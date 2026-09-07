ee4d28abee7d04027223185812b113f0ed32f5a8
MrAaron <aaronponcevi@gmail.com>
2026-09-07 15:23:16 -0500

    Avance de acabados 1

---
+++ src/data/proyect.ts
@@ -0,0 +1,46 @@
+export const projects = [
+    // Sanz solutions
+    {
+        name: "Sanz Solutions",
+        technologies: [
+            "Figma",
+            "Responsive Design",
+            "Astro",
+            "TypeScript",
+            "JavaScript",
+            "APIs REST",
+            "TailwindCss",
+            "Git",
+            "Postman",
+        ],
+        images: [
+            "image.1",
+            "image.2",
+            "image.3",
+        ],
+        explanation:"Participé en el desarrollo del sitio web corporativo de SanzSolutions, transformando los diseños de Figma en una interfaz moderna, responsive y optimizada para distintos dispositivos. Utilicé Astro para estructurar el proyecto y crear componentes reutilizables, TypeScript para la integración y manejo de conexiones con APIs, y JavaScript para implementar animaciones, interacciones y comportamientos dinámicos en el DOM. También colaboré en el mantenimiento, corrección de incidencias y control de versiones con Git." ,
+        link:"https://www.sanz.pe/",
+    },
+
+    // Zigbee Home
+    {
+        name: "Zigbee Home",
+        technologies: [
+            "Figma",
+            "Responsive Design",
+            "Astro",
+            "TypeScript",
+            "JavaScript",
+            "TailwindCss",
+            "Git",
+            "Vercel",
+        ],
+        images: [
+            "image.1",
+            "image.2",
+            "image.3",
+        ],
+        explanation:"Desarrollé de forma integral el sitio web de ZigbeeHome, desde la creación de su identidad visual hasta su publicación en producción. Diseñé en Figma el logo, la paleta de colores, la estructura visual, las secciones y los componentes de la interfaz; después transformé ese diseño en una web responsive utilizando Astro y Tailwind CSS. Implementé componentes reutilizables y archivos TypeScript para organizar el contenido y los datos iniciales del proyecto. También desarrollé formularios e interacciones para mejorar la experiencia de usuario, optimicé la estructura y rendimiento de la aplicación, y gestioné el despliegue y hosting del sitio en Vercel.",
+        link:"https://zigbee-home-s3k3.vercel.app/",
+    }
+]
